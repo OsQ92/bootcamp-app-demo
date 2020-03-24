f2ee0211516f421a764817dbaa0a5ed821f83dc5
Ossimies <okullervo@gmail.com>
2020-03-24 21:23:25 +0200

    Confirmation messagepopup for email. Needs still some styling.

--- frontend/src/Components/EmailForm.js
+++ frontend/src/Components/EmailForm.js
@@ -1,36 +1,71 @@
+//https://css-tricks.com/creating-animations-using-react-spring/
+
 import React from 'react';
+import MessageBox from './MessageBox';
 import './EmailForm.css';
 
 export default class EmailForm extends React.Component {
     state = {
-        firstname: "",
-        lastname: "",
-        message: ""
+        confirmed: false,
+        message: "",
+        showMessage: false,
+        email: {
+            firstname: "",
+            lastname: "",
+            subject: "",
+            message: ""
+        }
     }
     
     handleInputChange(event) {
         const target = event.target;
         const value = target.value;
         const inputName = target.name;
-        
-        this.setState({
-            [inputName]: value
-        });
-    }
 
+        this.setState({
+            email: {                   // object that we want to update
+                ...this.state.email,    // keep all other key-value pairs
+                [inputName]: value       // update the value of specific key
+            }
+        });
+        console.log(JSON.stringify(this.state.email));
+    }
+    
     onSave(event) {
         event.preventDefault();
-        fetch('/api/mail', {
+        fetch('/api/email', {
             method: 'POST',
             headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
             },
-            body: JSON.stringify(this.state)
+            body: JSON.stringify(this.state.email)
         })
-        .then(response => response.json())
-        .then(console.log(this.state))
-        .catch(err => console.log(err));
+        .then(response =>  response.json())
+        .then(data => {
+            if(data.accepted.length){ 
+                this.setState({
+                    confirmed: true, 
+                    message: "Message delivered", 
+                    showMessage: true}); 
+            } else { 
+                this.setState({
+                    confirmed: false, 
+                    message: "Message delivery failed", 
+                    showMessage: true
+                }); 
+            }
+        })
+        .catch(err => {
+            this.setState({
+                confirmed: false, 
+                message: "Message delivery failed", 
+                showMessage: true
+            });
+            console.log(err)});
+    }
+      closeModal() {
+        this.setState({ showMessage: false });
     }
 
     render() {
@@ -38,16 +73,33 @@ export default class EmailForm extends React.Component {
             <div className="email">
                 <form onSubmit={(event) => this.onSave(event)} >
                     <label>First Name</label>
-                    <input required type="text" id="fname" name="firstname" placeholder="Your name.." onChange={(event) => this.handleInputChange(event)}/>
+                    <input required type="text" id="fname" name="firstname" 
+                    placeholder="Your first name.." 
+                    onChange={(event) => this.handleInputChange(event)}/>
 
                     <label>Last Name</label>
-                    <input required type="text" id="lname" name="lastname" placeholder="Your last name.." onChange={(event) => this.handleInputChange(event)}/>
+                    <input required type="text" id="lname" name="lastname" 
+                    placeholder="Your last name.." 
+                    onChange={(event) => this.handleInputChange(event)}/>
+
+                    <label>Subject</label>
+                    <input required type="text" id="subject" name="subject" 
+                    placeholder="Title of your message.." 
+                    onChange={(event) => this.handleInputChange(event)}/>
 
                     <label>Message</label>
-                    <textarea required id="subject" name="message" placeholder="Write something.." onChange={(event) => this.handleInputChange(event)}></textarea>
-                    <button type="submit">Send</button>
+                    <textarea required id="subject" name="message" 
+                    placeholder="Write message.." 
+                    onChange={(event) => this.handleInputChange(event)}></textarea>
+                    
+                    <button type="submit" disabled={this.state.confirmed}>Send</button>      
                 </form>
+            <MessageBox 
+                message={this.state.message} 
+                open={this.state.showMessage} 
+                close={() => this.closeModal()}>
+            </MessageBox>
             </div>
-        );
+       );
     }
 }
---
+++ frontend/src/Components/MessageBox.js
@@ -0,0 +1,28 @@
+//https://react-popup.elazizi.com/introduction/
+import React from 'react';
+import Popup from 'reactjs-popup';
+import './MessageBox.css';
+
+export default class MessageBox extends React.Component {
+    render() {
+      return (
+        <div>
+          <Popup
+            open={this.props.open}
+            closeOnDocumentClick
+            onClose={this.props.close}
+          >
+            <div className="modal">
+              <div className="close" onClick={this.props.close}>
+                x
+              </div>
+              <div className="header">INFO</div>
+              <div className="content">
+                <span>{this.props.message}</span>
+              </div>
+            </div>
+          </Popup>
+        </div>
+      );
+    }
+  }
