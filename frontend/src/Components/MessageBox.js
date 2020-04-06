ba2b94931696d692efc24f5460c0c6cbd4e08c31
Ossimies <okullervo@gmail.com>
2020-04-06 16:44:58 +0300

    Messagebox styling by theme.

--- frontend/src/Components/MessageBox.js
+++ frontend/src/Components/MessageBox.js
@@ -1,27 +1,29 @@
 //https://react-popup.elazizi.com/introduction/
 import React from 'react';
 import Popup from 'reactjs-popup';
-import './MessageBox.css';
+import {MsgBoxContainer} from './MessageBox.styled'
 
 export default class MessageBox extends React.Component {
     render() {
       return (
         <div>
-          <Popup
-            open={this.props.open}
-            closeOnDocumentClick
-            onClose={this.props.close}
-          >
-            <div className="modal">
-              <div className="close" onClick={this.props.close}>
-                x
+          <MsgBoxContainer>
+            <Popup
+              open={this.props.open}
+              closeOnDocumentClick
+              onClose={this.props.close}
+            >
+              <div className="modal">
+                <div className="close" onClick={this.props.close}>
+                  x
+                </div>
+                <div className="header">INFO</div>
+                <div className="content">
+                  <span>{this.props.message}</span>
+                </div>
               </div>
-              <div className="header">INFO</div>
-              <div className="content">
-                <span>{this.props.message}</span>
-              </div>
-            </div>
-          </Popup>
+            </Popup>
+          </MsgBoxContainer>
         </div>
       );
     }
