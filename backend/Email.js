d3a4a6877a8f0bf5b73f639028457562b277e493
Ossimies <okullervo@gmail.com>
2020-04-07 12:11:09 +0300

    Move to env variables.

--- backend/Email.js
+++ backend/Email.js
@@ -1,19 +1,19 @@
 //This is a module to send email through Gmail.
 const nodemailer = require("nodemailer");
 const {google} = require("googleapis");
-const {CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URL, TARGET} = require('./EmailSettings.js');
+const {G_CLIENT_ID, G_CLIENT_SECRET, G_REFRESH_TOKEN, G_REDIRECT_URL, G_TARGET} = require('./EmailSettings.js');
 
 const OAuth2 = google.auth.OAuth2;
 
 const oauth2Client = new OAuth2(
-     CLIENT_ID,
-     CLIENT_SECRET,
-     REDIRECT_URL 
+     G_CLIENT_ID,
+     G_CLIENT_SECRET,
+     G_REDIRECT_URL 
 );
 
 //Get accesstoken with provided refreshtoken.
 oauth2Client.setCredentials({
-    refresh_token: REFRESH_TOKEN
+    refresh_token: G_REFRESH_TOKEN
 });
 const accessToken = oauth2Client.getAccessToken((error, response) => {
     if(error) {
@@ -30,17 +30,17 @@ const sendEmail = (req, res) => {
         service: "gmail",
         auth: {
              type: "OAuth2",
-             user: TARGET, 
-             clientId: CLIENT_ID,
-             clientSecret: CLIENT_SECRET,
-             refreshToken: REFRESH_TOKEN,
+             user: G_TARGET, 
+             clientId: G_CLIENT_ID,
+             clientSecret: G_CLIENT_SECRET,
+             refreshToken: G_REFRESH_TOKEN,
              accessToken: accessToken
         }
     });
    
     var message = {
         from: mail.sender,
-        to: TARGET,
+        to: G_TARGET,
         subject: mail.subject,
         text: mail.message
       };
--- backend/EmailSettings.js
+++ backend/EmailSettings.js
@@ -1,13 +1,13 @@
-const CLIENT_ID = '';
-const CLIENT_SECRET = '';
-const REFRESH_TOKEN = '';
-const REDIRECT_URL = 'https://developers.google.com/oauthplayground';
-const TARGET = 'okullervo@gmail.com';
+const G_CLIENT_ID = process.env.G_CLIENT_ID
+const G_CLIENT_SECRET = process.env.G_CLIENT_SECRET
+const G_REFRESH_TOKEN = process.env.G_REFRESH_TOKEN
+const G_REDIRECT_URL = process.env.G_REDIRECT_URL
+const G_TARGET = process.env.G_TARGET
 
 module.exports = {
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REFRESH_TOKEN,
-    REDIRECT_URL,
-    TARGET
+    G_CLIENT_ID,
+    G_CLIENT_SECRET,
+    G_REFRESH_TOKEN,
+    G_REDIRECT_URL,
+    G_TARGET
 };
